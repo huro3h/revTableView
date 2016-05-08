b72147c2b52c2a23f2c17ab682022b6fa501c21a
huro3h_satoshi <huro3h@gmail.com>
2016-05-08 09:58:27 +0800

    // ワンテンポ遅れて表示された原因 // tableView(tableView: UITableView, did "Des" electRowAtIndexPath indexPath: NSIndexPath) // になっていた。間違いやすい注意

--- revTableView/ViewController.swift
+++ revTableView/ViewController.swift
@@ -8,17 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+	@IBOutlet weak var myTableView: UITableView!
+	
+	var selectedIndex = -1
+	var sentoList = ["一の湯","御所の湯","まんだら湯","地蔵湯","鶴の湯"]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return sentoList.count
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell
+	{
+		var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+		cell.textLabel!.text = "\(sentoList[indexPath.row])"
+		return cell
+	}
+	
+	// ワンテンポ遅れて表示された原因
+	// tableView(tableView: UITableView, did"Des"electRowAtIndexPath indexPath: NSIndexPath)
+	// になっていた。間違いやすい注意
+	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		print("\(indexPath.row)行目の風呂を選択")
+		selectedIndex = indexPath.row
+		performSegueWithIdentifier("showsecView", sender: nil)
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		var secVC = segue.destinationViewController as! secViewController
+		// secVCにselected.Indexを書けばエラーが消えた
+		secVC.selectedIndex = selectedIndex
+		
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	override func didReceiveMemoryWarning() {
+//		super.didReceiveMemoryWarning()
+//		// Dispose of any resources that can be recreated.
+//	}
 
 
 }
---
+++ revTableView/secViewController.swift
@@ -0,0 +1,45 @@
+//
+//  secViewController.swift
+//  revTableView
+//
+//  Created by satoshiii on 2016/05/08.
+//  Copyright © 2016年 satoshiii. All rights reserved.
+//
+
+import UIKit
+
+class secViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	var selectedIndex = -1
+	
+	override func viewWillAppear(animated: Bool) {
+		print(selectedIndex)
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+
+}
